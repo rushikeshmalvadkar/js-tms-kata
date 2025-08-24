fc8b9388c219006182027b366af812730ce08c4f
Rushikeh <rushi@dev.com>
2025-08-24 12:48:51 +0530

    Added remove task feature with some cleanup

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// get the input from the add task input tag
+
 const taskList = [];
 const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
@@ -7,42 +7,47 @@ addTaskBtn.addEventListener("click", addTask);
 
 function addTask() {
     const task = taskInput.value.trim();
-    if (task === "") {
-        console.log("please enter the task");
-        return;
-    }
     taskList.push({ name: task, status: false });
     taskInput.value = "";
-    renderTasks(task);
+    renderTasks();
 }
 
-// show the task in hpme page
-function renderTasks(task) {    
+
+function renderTasks() {
     console.log(`current task list ${JSON.stringify(taskList)}`);
-    taskContainer.replaceChildren(); // clears all children
+    taskContainer.replaceChildren();
 
     for (let task of taskList) {
         const checkbox = document.createElement("input");
         const taskDiv = document.createElement("div");
+        const removeTaskBtn = document.createElement("button");
+        removeTaskBtn.textContent = "Remove";
         checkbox.type = "checkbox";
         checkbox.checked = task.status;
-        checkbox.addEventListener("click",() => handleTaskStatusChange(task));
+        checkbox.addEventListener("click", () => handleTaskStatusChange(task));
+        removeTaskBtn.addEventListener("click", () => handleRemoveTask(task));
         const p = document.createElement("p");
         p.textContent = task.name;
         taskContainer.appendChild(taskDiv);
         taskDiv.appendChild(checkbox);
         taskDiv.appendChild(p);
+        taskDiv.appendChild(removeTaskBtn);
     }
 }
 function handleTaskStatusChange(task) {
     console.log("<<<<<<<<<< handleTaskStatusChange");
-    
     const changedTask = taskList.find(t => t.name === task.name);
     console.log(`task to be change ${JSON.stringify(changedTask)}`);
-    
     changedTask.status = !changedTask.status;
     console.log(`current task list ${JSON.stringify(taskList)}`);
-    
     console.log("handleTaskStatusChange >>>>>>>>>>");
 }
+
+const handleRemoveTask = (task) => {
+    console.log("<<<<<<<<<< handleRemoveTask");
+    const taskIndexToBeRemove = taskList.indexOf(task);
+    taskList.splice(taskIndexToBeRemove, 1);
+    console.log("handleRemoveTask >>>>>>>>>>");
+    renderTasks();
+};
 
